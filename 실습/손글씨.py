49992b24d049f6455c2535ebd88ad0a2c3d10773
byeolla <fkrkty93@g.skku.edu>
2022-08-15 00:57:01 +0900

    dl_scratch - 08-15 study

---
+++ 실습/손글씨.py
@@ -0,0 +1,50 @@
+#%%
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+import numpy as np
+import pickle
+from dataset.mnist import load_mnist
+from common.functions import sigmoid, softmax
+#%%
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+      
+def get_data(): 
+    (x_train, t_train), (x_test, t_test) = \
+        load_mnist(flatten=True, normalize = False, one_hot_label= False)
+    
+    return x_test, t_test
+
+def init_network():
+    with open("../ch03/sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+        
+    return network
+
+def predict(network, x):
+    w1, w2, w3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
+    
+    a1 = np.dot(x,w1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1,w2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2,w3) + b3
+    y = softmax(a3)
+    
+    return y
+#%%
+x, t = get_data()
+network = init_network()
+# %%
+batch_size = 100
+accuracy_cnt = 0
+for i in range(0,len(x),batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis = 1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+# %%
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+# %%
